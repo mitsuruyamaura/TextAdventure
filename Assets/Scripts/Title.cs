48da7ea46d12948f8822dd470f846d7e5440da8f
mitsuru_yamaura <komayou326@gmail.com>
2020-10-12 18:57:46 +0900

    Title.csのStart内の処理をメソッド化。サマリー追加

--- Assets/Scripts/Title.cs
+++ Assets/Scripts/Title.cs
@@ -10,22 +10,38 @@ public class Title : MonoBehaviour
 
     public Button btnNewGame;
 
+    /// <summary>
+    /// エンディングを見た数の確認
+    /// </summary>
+    private void CheckEndingCount() {
+
+        // すべてのエンディングを見ているか確認。見ている場合には true 見ていない場合には false 
+        isNewGameButton = GameData.instance.LoadCheckEndingData();
+
+        // すべてのエンディングを見ている場合、新しいボタンを表示
+        if (isNewGameButton == true) {
+            btnNewGame.gameObject.SetActive(true);
+            btnNewGame.onClick.AddListener(OnClickNewGameButton);
+        }
+    }
+
+    /// <summary>
+    /// Gameシーンへ遷移
+    /// </summary>
     public void LoadMain() {
         SceneManager.LoadScene("Game");
     }
 
     void Start() {
-        // すべてのエンディングを見ているか確認。見ている場合には true 見ていない場合には false 
+        // 
         if (GameData.instance.endingCount > 0) {
-            isNewGameButton = GameData.instance.LoadCheckEndingData();
-        }
-
-        // すべてのエンディングを見ている場合、新しいボタンを表示
-        if (isNewGameButton == true) {
-            btnNewGame.gameObject.SetActive(true);
+            CheckEndingCount();
         }
     }
 
+    /// <summary>
+    /// エンディング・コンプリート時に追加されるボタンに登録する処理
+    /// </summary>
     public void OnClickNewGameButton() {
         // 新しいゲーム、エンディングのスタート内容を記述
 
